57b5ce59579506d2794d15c13c9b4ce45397d0be
Mohamed <hello.dateroulette@gmail.com>
2023-12-25 16:12:27 +0200

    changed title

--- pages/index.jsx
+++ pages/index.jsx
@@ -201,7 +201,7 @@ const Home = () => {
               <div className="text-center">
                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                   <span className="block">The free & opensource</span>
-                  <span className="hero-title block ">link in bio tool</span>
+                  <span className="hero-title block ">abc</span>
                 </h1>
                 <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                   Librelinks is an opensource link in bio tool that helps you
